bc74d90b1a62db442f62e6406b4cfdc9339d3af9
Ivan Veštić <ivestic@gmail.com>
2017-07-02 06:07:57 +0200

    cleanup

--- tests/Fixture/DummyBookFixture.php
+++ tests/Fixture/DummyBookFixture.php
@@ -6,26 +6,17 @@
 
 namespace IvanVestic\UtilityBelt\Tests\Fixture;
 
-
 use IvanVestic\UtilityBelt\Object\AbstractDataValueObject;
-use IvanVestic\UtilityBelt\Object\ObjectConstructionTrait;
 
 /**
  * Class DummyBookFixture
  */
 class DummyBookFixture extends AbstractDataValueObject
 {
-     use ObjectConstructionTrait;
-
      /** @var string */
      protected $title;
     /** @var string */
      protected $author;
-
-    public function __construct()
-    {
-        parent::__construct(...func_get_args());
-    }
 
     /**
      * @return string
